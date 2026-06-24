ba1c4279b1b813390b2006e4f6579f94619da576
Jorge Hernández <jhernandez@sicegroup.com>
2026-06-24 10:50:15 -0600

    fix cors definitivo

--- servermail/index.js
+++ servermail/index.js
@@ -9,11 +9,14 @@ const app = express();
 
 
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
+
+app.use(cors({
+  origin: "https://pagina-sice-group.pages.dev"
+}));
+``
+
+
 
 
 

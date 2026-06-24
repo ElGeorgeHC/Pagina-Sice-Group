7c36a4b9bbadb8c9ee6209a6a1326c2b2e0ce83a
Jorge Hernández <jhernandez@sicegroup.com>
2026-06-23 22:13:53 -0600

    fix CORS

--- servermail/index.js
+++ servermail/index.js
@@ -5,7 +5,11 @@ const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: "*"
+}));
+
 
 const upload = multer({ dest: "uploads/" });
 
